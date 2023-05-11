83a1bcb263e4748a4768bda85f2c1e879cd53d21
blackkspydo <upadhyaya13kishor@gmail.com>
2023-05-11 17:02:39 +0545

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,6 @@
 <script lang="ts">
 	// @ts-ignore
-	import { compile, parse } from 'mathjs';
-
-	function createFunction(expression: string) {
-		const parsed = parse(expression);
-		const compiled = compile(parsed as any);
-		return function (x: number) {
-			return compiled.evaluate({ x });
-		};
-	}
-
-	// Quadratic
-	const quadratic = '2*x^2 + 3*x + 1';
-	const f1 = createFunction(quadratic);
-	console.log(f1(2)); // 2*(2^2) + 3*2 + 1 = 17
-
-	// Exponential
-	const exponential = '3 * e^(2*x)';
-	const f2 = createFunction(exponential);
-	console.log(f2(2)); // 3 * e^(2*2) = 162.86245334760156
-
-	// Mixed
-	const mixed = 'x^3 - 2*sin(x) + e^x';
-	const f3 = createFunction(mixed);
-	console.log(f3(2)); // 2^3 - 2*sin(2) + e^2 = 12.778112197861301
-
-	// Trigonometric
-	const trigonometric = 'cos(x) - sin(x)';
-	const f4 = createFunction(trigonometric);
-	console.log(f4(Math.PI / 4)); // cos(pi/4) - sin(pi/4) = 0
-
-	let equation = '';
-	$: console.log(equation);
+	let equation: string = '';
 </script>
 
 <input
